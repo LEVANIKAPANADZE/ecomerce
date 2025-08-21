889461d1fba3fb38fdc282de0a02cfda9db579d2
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-08-21 19:36:03 +0400

    Worked on AudioTech component

--- src/components/AudioTechSel.tsx
+++ src/components/AudioTechSel.tsx
@@ -1,52 +1,71 @@
 import { Link } from "react-router-dom";
-import rightArrow from "/right-arrow.png";
+import rightArrow from "/fast-forward.png";
 
 export default function AudioTechSel() {
-  const navArr = [
+  type NavItem = {
+    path: string;
+    name: string;
+    image: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+    };
+  };
+
+  const navArr: NavItem[] = [
     {
       path: "/HEADPHONES",
       name: "HEADPHONES",
-      image:
-        "/assets/product-xx99-mark-one-headphones/tablet/image-category-page-preview.jpg",
+      image: {
+        mobile:
+          "/assets/product-xx99-mark-one-headphones/mobile/image-category-page-preview.jpg",
+        tablet:
+          "/assets/product-xx99-mark-one-headphones/tablet/image-category-page-preview.jpg",
+        desktop:
+          "/assets/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg",
+      },
     },
     {
       path: "/SPEAKERS",
       name: "SPEAKERS",
-      image:
-        "/assets/product-zx9-speaker/tablet/image-category-page-preview.jpg",
+      image: {
+        mobile:
+          "/assets/product-zx9-speaker/mobile/image-category-page-preview.jpg",
+        tablet:
+          "/assets/product-zx9-speaker/tablet/image-category-page-preview.jpg",
+        desktop:
+          "/assets/product-zx9-speaker/desktop/image-category-page-preview.jpg",
+      },
     },
     {
       path: "/EARPHONES",
       name: "EARPHONES",
-      image:
-        "/assets/product-yx1-earphones/tablet/image-category-page-preview.jpg",
+      image: {
+        mobile:
+          "/assets/product-yx1-earphones/mobile/image-category-page-preview.jpg",
+        tablet:
+          "/assets/product-yx1-earphones/tablet/image-category-page-preview.jpg",
+        desktop:
+          "/assets/product-yx1-earphones/desktop/image-category-page-preview.jpg",
+      },
     },
   ];
 
   return (
     <div>
-      {navArr.map((element) => {
-        return (
-          <Link key={element.name} to={`${element.path}`}>
-            <img
-              src={element.image}
-              alt="Audio tech's image"
-              className="w-[100px] h-[100px]"
-            />
-            <span>{element.name}</span>
-
-            <div>
-              {" "}
-              SHOP{" "}
-              <img
-                src={rightArrow}
-                alt="Arrow to right"
-                className="w-[100px] h-[100px]"
-              />
-            </div>
-          </Link>
-        );
-      })}
+      {navArr.map((item) => (
+        <Link key={item.name} to={item.path}>
+          <picture>
+            <source media="(min-width:1024px)" srcSet={item.image.desktop} />
+            <source media="(min-width:768px)" srcSet={item.image.tablet} />
+            <img src={item.image.mobile} alt={`${item.name} category`} />
+          </picture>
+          <span>{item.name}</span>
+          <div>
+            SHOP <img src={rightArrow} alt="Arrow to right" />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
