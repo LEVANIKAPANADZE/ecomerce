0190aed452e1d5a579cfa7611a0b7a90606024b1
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-10-31 11:50:36 +0400

    Finished tablet design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,26 +8,27 @@ export default function Header() {
   return (
     <div>
       <header
-        className="bg-black text-white flex justify-between 
-      items-center px-[24px] 
-      py-[32px] border-b-2 border-white/12"
+        className="bg-black text-white flex justify-between items-center 
+    px-[24px] py-[32px] border-b-3 border-white/10
+    md:px-[40px] md:py-[34px] md:justify-start md:gap-6"
       >
-        <div>
+        <div className="flex items-center gap-4 md:gap-6">
           <img
             src="/menu.png"
             alt="Burger Menu"
-            className="h-[27px] w-[27px] cursor-pointer"
+            className="h-[27px] w-[27px] cursor-pointer md:h-[35px] md:w-[35px]"
             onClick={() => setBurger(!burger)}
           />
+          <h1 className="font-black text-[20px] md:text-[23px]">audiophile</h1>
         </div>
 
-        <h1 className="font-black text-[20px]">audiophile</h1>
-
-        <img
-          src={shoppingCart}
-          alt="Shopping Cart"
-          className="h-[23px] w-[23px] cursor-pointer"
-        />
+        <div className="ml-auto">
+          <img
+            src={shoppingCart}
+            alt="Shopping Cart"
+            className="h-[23px] w-[23px] cursor-pointer md:h-[31px] md:w-[31px]"
+          />
+        </div>
       </header>
 
       <div className="border-b-2 border-white">
