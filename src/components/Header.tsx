d93e5295b3e61256bdf7c049c207644c3145c157
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-11-04 21:39:38 +0400

    Adding css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,10 @@ export default function Header() {
   return (
     <div>
       <header
-        className="bg-black text-white flex justify-between items-center 
-    px-[24px] py-[32px] border-b-3 border-white/10
-    md:px-[40px] md:py-[34px] md:justify-start md:gap-6"
+        className="bg-black text-white flex justify-between 
+        items-center 
+        px-[24px] py-[32px] border-b-3 border-white/10
+        md:px-[40px] md:py-[34px] md:justify-start md:gap-6"
       >
         <div className="flex items-center gap-4 md:gap-6">
           <img
