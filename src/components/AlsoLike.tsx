aea2543c07253644c340a67b03f3809edd9a5c09
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-01-01 18:07:07 +0400

    Made whole AlsoLike component only css left

--- src/components/AlsoLike.tsx
+++ src/components/AlsoLike.tsx
@@ -11,7 +11,14 @@ export default function AlsoLike({ product }: Props) {
       <h1>YOU MAY ALSO LIKE</h1>
       {product.others.map((element, index) => (
         <div key={index}>
-          <img src={element.image.mobile} alt={element.name} />
+          <picture key={index}>
+            <source media="(min-width:1440px)" srcSet={element.image.desktop} />
+            <source media="(min-width:768px)" srcSet={element.image.tablet} />
+            <img
+              src={element.image.mobile}
+              alt={`Product image ${index + 1}`}
+            />
+          </picture>
           <h1>{element.name}</h1>
           <Link to={`/product?slug=${encodeURIComponent(element.slug)}`}>
             SEE PRODUCT
