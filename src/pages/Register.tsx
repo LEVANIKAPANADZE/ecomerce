8d7301889f12ec9a505476cb633a4c7002867a9b
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-03-03 17:14:28 +0400

    Deleted old api

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -62,20 +62,17 @@ export default function Register() {
 
     if (!hasErrors) {
       try {
-        const response = await fetch(
-          "https://pixelize.pythonanywhere.com/users/",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              username: trimmedValues.Username,
-              email: trimmedValues.Email,
-              password: trimmedValues.Password,
-            }),
-          }
-        );
+        const response = await fetch("", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: trimmedValues.Username,
+            email: trimmedValues.Email,
+            password: trimmedValues.Password,
+          }),
+        });
         const data = await response.json();
         console.log(data);
         navigate("/Home", { state: { id: data.id } });
